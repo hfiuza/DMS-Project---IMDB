08f9fefa77779c41af61d661b769c865d1607373
Diego de Souza <dieggoluis@gmail.com>
2016-10-28 16:48:15 +0200

    [DEV] Add script to measure the execution time of a query

---
+++ timer/run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# usage
+if [ -z "$1" -o -z "$2" -o "$1" = "-h" -o "$1" = "help" ]; then 
+    echo usage: $0 "query_number {query, explain}" 
+    echo ex1: $0 1 query
+    echo ex2: $0 2 explain
+    exit
+fi
+
+# must change these settings for each machine
+# -------------------------------------------
+PSQL=./psql
+QUERY_DIR=../queries
+DATABASE=inf553
+QUERY_NUM=$1
+# mac users (gdate)
+# linux users (date)
+DATE_CMD=gdate
+# -------------------------------------------
+
+# source and output files
+QUERY_SRC=q$1.sql
+QUERY_OUT=q$1.out
+QUERY_EXPLAIN=explain$1.out
+
+# save query or explain output and calculate its execution time
+if [ "$2" = explain ]; then
+    rm -f $QUERY_DIR/$QUERY_EXPLAIN
+    touch tmp.sql
+    echo "EXPLAIN " >> tmp.sql
+    cat $QUERY_DIR/$QUERY_SRC >> tmp.sql
+    $PSQL $DATABASE -f tmp.sql > $QUERY_DIR/$QUERY_EXPLAIN
+    rm -f tmp.sql
+else
+    rm -f $QUERY_DIR/$QUERY_OUT
+    START=$($DATE_CMD +%s.%N)
+    $PSQL $DATABASE -f $QUERY_DIR/$QUERY_SRC > $QUERY_DIR/$QUERY_OUT
+    END=$($DATE_CMD +%s.%N)
+    DIFF=$(echo "$END - $START" | bc)
+    echo $DIFF
+fi
+
+
