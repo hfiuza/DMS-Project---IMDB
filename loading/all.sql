5112fc79e2c67d22c55a6e2af64f3e7d8069a05a
Diego de Souza <dieggoluis@gmail.com>
2016-10-29 22:11:28 +0200

    [DEV] Add inputdir variable to loading/all.sql script

--- loading/all.sql
+++ loading/all.sql
@@ -1,11 +1,14 @@
+
+\set inputdir '\'/home/hfiuza/Desktop/Polytechnique/DMS/data'
+
 CREATE TABLE public.movie_type
 (
   id integer NOT NULL,
   kind character varying(50),
   CONSTRAINT movie_type5_pkey PRIMARY KEY (id)
-)
+);
 
-copy movie_type from '/home/hfiuza/Desktop/Polytechnique/DMS/data/movie_type.csv' DELIMITER ',' HEADER CSV
+copy movie_type from :inputdir/movie_type.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE info_type(
 	id INT NOT NULL,
@@ -13,7 +16,7 @@ CREATE TABLE info_type(
 	PRIMARY KEY(id)
 );
 
-copy info_type from '/home/hfiuza/Desktop/Polytechnique/DMS/data/info_type.csv' DELIMITER ',' HEADER CSV
+copy info_type from :inputdir/info_type.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE keyword(
 	id INT,
@@ -21,7 +24,7 @@ CREATE TABLE keyword(
 	PRIMARY KEY(id)
 );
 
-copy keyword from '/home/hfiuza/Desktop/Polytechnique/DMS/data/keyword.csv' DELIMITER ',' HEADER CSV
+copy keyword from :inputdir/keyword.csv' DELIMITER ',' HEADER CSV;
 
 /* reading table person */
 CREATE TABLE person(
@@ -30,7 +33,8 @@ CREATE TABLE person(
 	gender CHAR,
 	PRIMARY KEY(id)
 );
-copy person from '/home/hfiuza/Desktop/Polytechnique/DMS/data/person.csv' DELIMITER ',' HEADER CSV
+
+copy person from :inputdir/person.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE company(
 	id INT NOT NULL PRIMARY KEY,
@@ -38,7 +42,7 @@ CREATE TABLE company(
 	country_code VARCHAR(8)
 );
 
-copy company from '/home/hfiuza/Desktop/Polytechnique/DMS/data/company.csv' DELIMITER ',' HEADER CSV
+copy company from :inputdir/company.csv' DELIMITER ',' HEADER CSV;
 
 /* more movie.csv | cut -d ',' -f2 | wc -L */
 
@@ -53,7 +57,7 @@ CREATE TABLE movie(
 	series_years VARCHAR(10)
 );
 
-copy movie from '/home/hfiuza/Desktop/Polytechnique/DMS/data/movie.csv' DELIMITER ',' HEADER CSV
+copy movie from :inputdir/movie.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE aka_name(
 	id INT NOT NULL PRIMARY KEY,
@@ -61,7 +65,7 @@ CREATE TABLE aka_name(
 	name VARCHAR(250)
 );
 
-copy aka_name from '/home/hfiuza/Desktop/Polytechnique/DMS/data/aka_name.csv' DELIMITER ',' HEADER CSV
+copy aka_name from :inputdir/aka_name.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE aka_title(
 	id INT NOT NULL PRIMARY KEY,
@@ -80,9 +84,7 @@ CREATE TABLE aka_title(
 ALTER TABLE aka_title
 DROP CONSTRAINT movie_fk
 
-
-copy aka_title from '/home/hfiuza/Desktop/Polytechnique/DMS/data/aka_title.csv' DELIMITER ',' HEADER CSV
-
+copy aka_title from :inputdir/aka_title.csv' DELIMITER ',' HEADER CSV;
 
 DELETE FROM aka_title
 WHERE
@@ -97,9 +99,9 @@ REFERENCES movie(id) ON DELETE CASCADE ON UPDATE CASCADE
 CREATE TABLE link_type(
 	id INT NOT NULL PRIMARY KEY,
 	link VARCHAR(40) NOT NULL
-)
+);
 	
-copy link_type from '/home/hfiuza/Desktop/Polytechnique/DMS/data/link_type.csv' DELIMITER ',' HEADER CSV
+copy link_type from :inputdir/link_type.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE movie_link(
 	id INT NOT NULL PRIMARY KEY,
@@ -108,7 +110,7 @@ CREATE TABLE movie_link(
 	link_type_id INT NOT NULL REFERENCES link_type(id)
 );
 
-copy movie_link from '/home/hfiuza/Desktop/Polytechnique/DMS/data/movie_link.csv' DELIMITER ',' HEADER CSV
+copy movie_link from :inputdir/movie_link.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE movie_rating(
 	id INT NOT NULL PRIMARY KEY,
@@ -117,21 +119,21 @@ CREATE TABLE movie_rating(
 	info VARCHAR(30) NOT NULL
 );
 
-copy movie_rating from '/home/hfiuza/Desktop/Polytechnique/DMS/data/movie_rating.csv' DELIMITER ',' HEADER CSV
+copy movie_rating from :inputdir/movie_rating.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE char_name(
 	id INT NOT NULL PRIMARY KEY,
 	name VARCHAR(500) NOT NULL
 );
 
-copy char_name from '/home/hfiuza/Desktop/Polytechnique/DMS/data/char_name.csv' DELIMITER ',' HEADER CSV
+copy char_name from :inputdir/char_name.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE comp_cast_type(
 	id INT NOT NULL PRIMARY KEY,
 	name VARCHAR(20) NOT NULL
 );
 
-copy comp_cast_type from '/home/hfiuza/Desktop/Polytechnique/DMS/data/comp_cast_type.csv' DELIMITER ',' HEADER CSV
+copy comp_cast_type from :inputdir/comp_cast_type.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE person_info(
 	id INT NOT NULL PRIMARY KEY,
@@ -141,14 +143,14 @@ CREATE TABLE person_info(
 	note VARCHAR(400)
 );
 
-copy person_info from '/home/hfiuza/Desktop/Polytechnique/DMS/data/person_info.csv' DELIMITER ',' HEADER CSV
+copy person_info from :inputdir/person_info.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE role_type(
 	id INT NOT NULL PRIMARY KEY,
 	role VARCHAR(20)
 );
 
-copy role_type from '/home/hfiuza/Desktop/Polytechnique/DMS/data/role_type.csv' DELIMITER ',' HEADER CSV
+copy role_type from :inputdir/role_type.csv' DELIMITER ',' HEADER CSV;
 
 
 CREATE TABLE cast_info(
@@ -160,14 +162,14 @@ CREATE TABLE cast_info(
 	role_id INT NOT NULL REFERENCES role_type(id)
 );
 
-copy cast_info from '/home/hfiuza/Desktop/Polytechnique/DMS/data/cast_info.csv' DELIMITER ',' HEADER CSV
+copy cast_info from :inputdir/cast_info.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE company_type(
 	id INT NOT NULL PRIMARY KEY,
 	kind VARCHAR(30) NOT NULL
 );
 
-copy company_type from '/home/hfiuza/Desktop/Polytechnique/DMS/data/company_type.csv' DELIMITER ',' HEADER CSV
+copy company_type from :inputdir/company_type.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE movie_company(
 	id INT NOT NULL PRIMARY KEY,
@@ -177,7 +179,7 @@ CREATE TABLE movie_company(
 	note VARCHAR(250)
 );
 
-copy movie_company from '/home/hfiuza/Desktop/Polytechnique/DMS/data/movie_company.csv' DELIMITER ',' HEADER CSV
+copy movie_company from :inputdir/movie_company.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE movie_info(
 	id INT NOT NULL PRIMARY KEY,
@@ -187,7 +189,7 @@ CREATE TABLE movie_info(
 	note VARCHAR(35200)
 );
 
-copy movie_info from '/home/hfiuza/Desktop/Polytechnique/DMS/data/movie_info.csv' DELIMITER ',' HEADER CSV
+copy movie_info from :inputdir/movie_info.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE complete_cast(
 	id INT NOT NULL PRIMARY KEY,
@@ -196,7 +198,7 @@ CREATE TABLE complete_cast(
 	status_id INT NOT NULL REFERENCES comp_cast_type ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
-copy complete_cast from '/home/hfiuza/Desktop/Polytechnique/DMS/data/complete_cast.csv' DELIMITER ',' HEADER CSV
+copy complete_cast from :inputdir/complete_cast.csv' DELIMITER ',' HEADER CSV;
 
 CREATE TABLE  movie_keyword(
 	id INT NOT NULL PRIMARY KEY,
@@ -204,6 +206,6 @@ CREATE TABLE  movie_keyword(
 	keyword_id INT NOT NULL REFERENCES keyword(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-copy movie_keyword from '/home/hfiuza/Desktop/Polytechnique/DMS/data/movie_keyword.csv' DELIMITER ',' HEADER CSV
+copy movie_keyword from :inputdir/movie_keyword.csv' DELIMITER ',' HEADER CSV;
 
 
