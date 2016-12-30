698893a1fbe6e50c28af9b476e645c8df216d1ed
hfiuza <henriquegfn@gmail.com>
2016-12-30 20:13:04 +0100

    reviewed code and fixed queries 6 and 7

--- queries/q6.sql
+++ queries/q6.sql
@@ -7,12 +7,36 @@ Find the titles of the movies that have only 1 and 10 as ratings, and order them
 	Actual time: 1609.60 ms
 */
 
-
+/*
 SELECT movie.title, valid_movie_rating.average
-FROM movie,  (SELECT only_movie_rating.movie_id, (COUNT (CASE WHEN only_movie_rating.info<>'1.0' AND only_movie_rating.info<>'10.0' THEN 1 ELSE NULL END) ) AS is_not_valid, ( ( (COUNT (CASE WHEN only_movie_rating.info='1.0' THEN 1 ELSE NULL END )) + (COUNT (CASE WHEN only_movie_rating.info='10.0' THEN 1 ELSE NULL END ))*10 )/(COUNT (*)) ) AS average /* is_not_valid contains the number of ratings that a movie receives that are different from 1.0 and 10.0.*/
-	FROM (SELECT movie_rating.movie_id AS movie_id, movie_rating.info AS info FROM movie_rating, info_type WHERE info_type.info='rating' AND movie_rating.info_type_id=info_type.id) AS only_movie_rating /* we only consider lines from movie_rating that contain ratings, whose info_type_id is 101*/
+FROM movie,  (SELECT only_movie_rating.movie_id, (COUNT (CASE WHEN only_movie_rating.info<>'1.0' AND only_movie_rating.info<>'10.0' THEN 1 ELSE NULL END) ) AS is_not_valid, ( ( (COUNT (CASE WHEN only_movie_rating.info='1.0' THEN 1 ELSE NULL END )) + (COUNT (CASE WHEN only_movie_rating.info='10.0' THEN 1 ELSE NULL END ))*10 )/(COUNT (*)) ) AS average
+	FROM (SELECT movie_rating.movie_id AS movie_id, movie_rating.info AS info FROM movie_rating, info_type WHERE info_type.info='rating' AND movie_rating.info_type_id=info_type.id) AS only_movie_rating 
 	GROUP BY movie_id
 ) AS valid_movie_rating
-WHERE movie.id=valid_movie_rating.movie_id AND valid_movie_rating.is_not_valid=0 /* we only select movies that have is_not_valid equal to 0*/
+WHERE movie.id=valid_movie_rating.movie_id AND valid_movie_rating.is_not_valid=0
 ORDER BY valid_movie_rating.average DESC;
+*/
 
+
+SELECT movie.title
+FROM (SELECT DISTINCT movie_rating.movie_id AS id
+			FROM movie_rating, info_type
+			WHERE info_type.info='votes distribution' AND info_type.id=movie_rating.info_type_id AND movie_rating.info LIKE '_........_'
+			) AS good_movies,
+			( SELECT movie_rating.movie_id, movie_rating.info_type_id, CAST(movie_rating.info AS FLOAT) _info
+				FROM movie_rating			
+			) AS decimal_movie_rating, movie, info_type
+WHERE good_movies.id=decimal_movie_rating.movie_id AND decimal_movie_rating.movie_id=movie.id AND info_type.info='rating' AND info_type.id=decimal_movie_rating.info_type_id
+ORDER BY decimal_movie_rating._info DESC
+
+/*
+SELECT M.title
+FROM movie M, movie_rating MR, info_type IT
+WHERE MR.info LIKE '_........_' AND IT.info = 'votes distribution' AND IT.id = MR.info_type_id AND M.id = MR.movie_id
+ORDER BY LEFT(MR.info, 1) DESC, RIGHT(MR.info, 1) DESC;
+*/
+/*
+SELECT *
+FROM movie_rating mr1, movie_rating mr2, info_type
+WHERE info_type.info='votes distribution' AND info_type.id=mr1.info_type_id AND info_type.id=mr2.info_type_id AND mr1.movie_id=mr2.movie_id AND mr1.id<>mr2.id
+*/
--- queries/q7.sql
+++ queries/q7.sql
@@ -21,7 +21,7 @@ FROM (SELECT COUNT(movie.production_year) AS amount
 
 /*
     Considering only movies of the type "movie"
-    Total time: 15.059945000
+    Actual time: 15.059945000
 */
 
 SELECT SUM(number_of_movies_by_year.amount)/COUNT(number_of_movies_by_year)
@@ -37,3 +37,20 @@ FROM (SELECT COUNT(movie.production_year) AS amount
       GROUP BY movie.production_year
      ) AS number_of_movies_by_year
 
+
+/*
+
+Helpful code to gain insight of the data
+
+SELECT number_of_movies_by_year.production_year, number_of_movies_by_year.amount
+FROM (SELECT COUNT(movie.production_year) AS amount, movie.production_year
+      FROM (SELECT DISTINCT cast_info.movie_id
+            FROM cast_info, person
+            WHERE person.name = 'Fonzi, Dolores' AND cast_info.person_id = person.id
+           ) AS movies_count,
+           movie, movie_type
+      WHERE movie_type.kind = 'movie' AND movie_type.id = movie.kind_id 
+            AND movie.id = movies_count.movie_id AND movie.production_year IS NOT NULL
+      GROUP BY movie.production_year
+     ) AS number_of_movies_by_year
+*
